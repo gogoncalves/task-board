75936589b647933ed728819e6290bbd851d882ea
gogoncalves <gustavo.goncalves@sptech.school>
2024-09-15 21:04:29 -0300

    feat: base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "@/styles/home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tasks+</title>
+      </Head>
+
+      <h1>My project</h1>
+    </>
+  );
+}
