00b30b7246ecd7e164631f8719e34ebe4d967aa0
gogoncalves <gustavo.goncalves@sptech.school>
2024-09-16 01:27:14 -0300

    feat: adding admin route

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import styles from "@/pages/dashboard/styles.module.css";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 
 export default function Dashboard(){
@@ -9,3 +11,20 @@ export default function Dashboard(){
         </div>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+
+    if(!session?.user){
+        return{
+            redirect:{
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    return{
+        props: {},
+    };
+};
