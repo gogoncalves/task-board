b9f9004685c29d73506b1e4d30541b1305039b61
gogoncalves <gustavo.goncalves@sptech.school>
2024-09-15 21:59:26 -0300

    feat: adding project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
-import styles from "@/styles/home.module.css";
+import Image from "next/image";
+import styles from "@/../../styles/home.module.css";
+import heroImg from "@/../public/assets/hero.png";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Tasks+</title>
       </Head>
 
-      <h1>My project</h1>
-    </>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image 
+            className={styles.hero}
+            alt="Task+ Logo"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas!
+        </h1>
+      </main>
+    </div>
   );
 }
